f6e2af36e947e1bf74032fbd6ab21b214e39ecc6
PearlR <pearl@techtank.org.nz>
2016-12-11 12:22:29 +1300

    add base app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import App from './components/App'
+import { reducer } from './state/reducer'
+
 import './index.css'
 
+const store = createStore(
+  reducer,
+  window.devToolsExtension && window.devToolsExtension()
+)
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
