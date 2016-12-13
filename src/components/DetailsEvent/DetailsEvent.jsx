15f6944783f761af12d0db4d22ffbd03f729235d
PearlR <pearl@techtank.org.nz>
2016-12-14 12:58:52 +1300

    add details event react bootstrap html elements

--- src/components/DetailsEvent/DetailsEvent.jsx
+++ src/components/DetailsEvent/DetailsEvent.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router'
+import {
+  Button,
+  ButtonGroup,
+  FormGroup,
+  FormControl,
+} from 'react-bootstrap'
 
 const DetailsEvent = () => {
   return <div>
-    <h1>Event Details</h1>
-    <p>Event Name</p>
-    <Link to='/invites'>Next</Link>
+    <FormGroup>
+    <h1>Important Details</h1>
+      <FormControl type='text' name='date' placeholder='date' />
+      <FormControl type='text' name='time' placeholder='time' />
+      <FormControl type='text' name='location' placeholder='location' />
+      <FormControl componentClass='textarea' name='description' placeholder='description' />
+      <ButtonGroup>
+        <Button><Link to='/invites'>Next</Link></Button>
+      </ButtonGroup>
+    </FormGroup>
   </div>
 }
 
