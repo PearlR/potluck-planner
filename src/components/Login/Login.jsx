4117897a76e297181c92c5a471fd4babd92d7428
PearlR <pearl@techtank.org.nz>
2016-12-14 12:38:19 +1300

    add login react bootstrap html elements

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router'
+import { Button, ButtonGroup, FormControl, FormGroup } from 'react-bootstrap'
 
 const Login = () => {
-  return <Link to="success">Login</Link>
+  return <div>
+  <FormGroup>
+      <h1>Login</h1>
+      <FormControl type='text' name='username' placeholder='username'></FormControl>
+      <FormControl type='text' name='password' placeholder='username'></FormControl>
+      <ButtonGroup>
+        <Button><Link to="success">Login</Link></Button>
+      </ButtonGroup>
+  </FormGroup>
+  </div>
 }
 
 export default Login
