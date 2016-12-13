a557c909cc0b77bb178acec262c8e0639e88c1ed
PearlR <pearl@techtank.org.nz>
2016-12-14 12:48:08 +1300

    add create event react bootstrap html elements

--- src/components/CreateEvent/CreateEvent.jsx
+++ src/components/CreateEvent/CreateEvent.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router'
+import { Button, ButtonGroup, FormGroup, FormControl } from 'react-bootstrap'
 
 const CreateEvent = () => {
   return <div>
+    <FormGroup>
       <h1>Let's create a potluck!</h1>
-      <Link to='/event-details'>Let's go!</Link>
+      <FormControl type='text' name='eventTitle' placeholder='eventTitle' />
+      <ButtonGroup>
+        <Button><Link to='/event-details'>Let's go!</Link></Button>
+      </ButtonGroup>
+    </FormGroup>
   </div>
 }
 
