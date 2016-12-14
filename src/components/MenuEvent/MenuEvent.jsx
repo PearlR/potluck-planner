28b6ace9ceb4bcb77dfcd9db95ae8233a7bb4e4d
PearlR <pearl@techtank.org.nz>
2016-12-14 13:15:13 +1300

    add menu event react bootstrap html elements

--- src/components/MenuEvent/MenuEvent.jsx
+++ src/components/MenuEvent/MenuEvent.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router'
+import { Button, ButtonGroup, FormGroup, FormControl } from 'react-bootstrap'
 
 const MenuEvent = () => {
   return <div>
-    <h1>What is everyone eating?</h1>
-    <Link to="/single-event">Next</Link>
+    <FormGroup>
+      <h1>What is everyone eating?</h1>
+        <FormControl type='text' name='category' placeholder='course' />
+        <FormControl type='text' name='dishName' placeholder='dish name' />
+        <FormControl type='text' name='userOfDish' placeholder='person assigned to dish' />
+      <ButtonGroup>
+        <Button><Link to="/single-event">Next</Link></Button>
+      </ButtonGroup>
+    </FormGroup>
   </div>
 }
 
